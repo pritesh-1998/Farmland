631e725ce4b087f8fe002b2766cb2b4c870daa70
Pritesh lathia <72033803+pritesh-1998@users.noreply.github.com>
2024-05-19 23:19:24 +0530

    vv

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -3,30 +3,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Farmland - Tailwind CSS Landing Page Demo</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" /> 
+    <title>Farmland - Farm Manager</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
 
     <!-- Meta SEO -->
     <meta name="title" content="Farmland - Tailwind CSS Landing Page">
-    <meta name="description" content="Get started with a free and open-source landing page built with Tailwind CSS and the Flowbite component library.">
+    <meta name="description"
+        content="Get started with a free and open-source landing page built with Tailwind CSS and the Flowbite component library.">
     <meta name="robots" content="index, follow">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="language" content="English">
-    <meta name="author" content="Themesberg"> <link
-    href="https://cdn.jsdelivr.net/npm/daisyui@2.6.0/dist/full.css"
-    rel="stylesheet"
-    type="text/css"
-  />
-  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <meta name="author" content="Themesberg">
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@2.6.0/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-  <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
     <!-- Social media share -->
     <meta property="og:title" content="Farmland - Tailwind CSS Landing Page">
     <meta property="og:site_name" content="Themesberg">
     <meta property="og:url" content="https://https://demo.themesberg.com/Farmland/">
-    <meta property="og:description" content="Get started with a free and open-source landing page for Tailwind CSS built with the Flowbite component library featuring dark mode, hero sections, pricing cards, and more.">
+    <meta property="og:description"
+        content="Get started with a free and open-source landing page for Tailwind CSS built with the Flowbite component library featuring dark mode, hero sections, pricing cards, and more.">
     <meta property="og:type" content="">
-    <meta property="og:image" content="https://themesberg.s3.us-east-2.amazonaws.com/public/github/Farmland/og-image.png">
+    <meta property="og:image"
+        content="https://themesberg.s3.us-east-2.amazonaws.com/public/github/Farmland/og-image.png">
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:site" content="@themesberg" />
     <meta name="twitter:creator" content="@themesberg" />
